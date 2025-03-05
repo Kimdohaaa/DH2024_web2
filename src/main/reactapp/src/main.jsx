6df9b2cede23b18ae5cd97af4a1cac25d139571f
kimdohaaa <jjeenee0408@gmail.com>
2025-03-05 11:18:23 +0900

    250305/ react 컴포넌트

--- src/main/reactapp/src/main.jsx
+++ src/main/reactapp/src/main.jsx
@@ -12,6 +12,8 @@ createRoot(document.getElementById('root')).render(
 )
 */
 
+// ※※※ day01 ※※※ //
+
 // [1] 컴포넌트 파일의 확장자를 js 로 지정할 경우
 import React from "react"; // -> 를 통해 명시해야함
   // 컴포넌트 파일의 확장자가 .jsx 일 경우 생략 가능
@@ -20,21 +22,23 @@ import React from "react"; // -> 를 통해 명시해야함
 const root = createRoot( document.querySelector("#root"));
 
 // [3] import 호출할 컴포넌트명 from `해당 컴포넌트 경로`
-import Component1 from './example/1_동작구조/Component1.jsx'; 
+// import Component1 from './example/1_동작구조/Component1.jsx'; 
 
 // root.render(<Component1/>) // !!! 마크업 슬래스로 꼭 닫아주기 !!!
-
-
-//////////////////
 
 
 // 기본 컴포넌트 //
 
 // [1] 컴포넌트 호출
-import Component2 from './example/Component2.jsx';
+// import Component2 from './example/1_동적구조/Component2.jsx';
 
 // [2] 렌더링 ! 렌더링은 한번만 !
   // 기본컴포넌트인 Component2 에 Header 컴포넌트와 Footer 컴포넌트가 종속되어 있음
-root.render(<Component2/>)
+// root.render(<Component2/>)
 
-// 그 외 컴포넌트 //
+
+
+// ※※※ day02 : 컴포넌트의 JSX 활용 ※※※ //
+// 컴포넌트 불러오기
+import Example1 from '/DH2024_WEB2/main/reactapp/src/example/2_컴포넌트/Example1.jsx';
+root.render(<Example1/>)
